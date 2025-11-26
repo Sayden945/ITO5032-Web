4eaa1b942a0c3c1ba60fe5c66931ac646c977f70
Ayden Smith <122020846+Sayden945@users.noreply.github.com>
2025-11-26 14:41:16 +1100

    Add routing configuration for HomeView component

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+const routes = [
+  {
+    home: '/',
+    name: 'home',
+    component: () => import('../views/HomeView.vue'),
+  },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 })
 
 export default router
