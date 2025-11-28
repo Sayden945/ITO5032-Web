77c785cc311a1aab027731a409fd32e498e8d9b9
Ayden Smith <122020846+Sayden945@users.noreply.github.com>
2025-11-28 11:41:57 +1100

    Implement more routes to Navbar

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,50 +1,21 @@
 <template>
-  <div class="container">
-    <header class="d-flex justify-content-center py-3">
+  <div class="sticky-top">
+    <header class="d-flex justify-content-center py-3 bg-dark">
       <ul class="nav nav-pills">
         <li class="nav-item">
-          <router-link to="/" class="nav-link" active-class="active" aria-current="page"
-            >Home</router-link
-          >
+          <router-link to="/" class="nav-link" active-class="active">Home</router-link>
         </li>
+        <li class="nav-item">
+          <router-link to="/About" class="nav-link" active-class="active">About Us</router-link>
+        </li>
+        <div class="col mg-1 justify-content-end">
+          <li class="nav-item">
+            <router-link to="/Account" class="nav-link" active-class="active">Account</router-link>
+          </li>
+        </div>
       </ul>
     </header>
   </div>
 </template>
 
-<style scoped>
-.b-example-divider {
-  height: 3rem;
-  background-color: rgba(0, 0, 0, 0.1);
-  border: solid rgba(0, 0, 0, 0.15);
-  border-width: 1px 0;
-  box-shadow:
-    inset 0 0.5em 1.5em rgba(0, 0, 0, 0.1),
-    inset 0 0.125em 0.5em rgba(0, 0, 0, 0.15);
-}
-
-.form-control-dark {
-  color: #fff;
-  background-color: var(--bs-dark);
-  border-color: var(--bs-gray);
-}
-.form-control-dark:focus {
-  color: #fff;
-  background-color: var(--bs-dark);
-  border-color: #fff;
-  box-shadow: 0 0 0 0.25rem rgba(255, 255, 255, 0.25);
-}
-
-.bi {
-  vertical-align: -0.125em;
-  fill: currentColor;
-}
-
-.text-small {
-  font-size: 85%;
-}
-
-.dropdown-toggle {
-  outline: 0;
-}
-</style>
+<style scoped></style>
